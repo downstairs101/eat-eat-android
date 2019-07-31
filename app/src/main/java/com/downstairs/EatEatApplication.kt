792443733bfdf777e2daeaebe82d867c559fc008
Roubert Edgar <roubertedgar@gmail.com>
2019-07-31 13:26:45 -0300

    Updates to new dagger android injector

--- app/src/main/java/com/downstairs/EatEatApplication.kt
+++ app/src/main/java/com/downstairs/EatEatApplication.kt
@@ -1,20 +1,22 @@
 package com.downstairs
 
-import android.app.Activity
 import android.app.Application
 import com.downstairs.injection.DaggerAppComponent
+import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class EatEatApplication : Application(), HasActivityInjector {
+class EatEatApplication : Application(), HasAndroidInjector {
     @Inject
-    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
         DaggerAppComponent.factory().create(applicationContext).inject(this)
     }
 
-    override fun activityInjector() = activityInjector
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+
 }
