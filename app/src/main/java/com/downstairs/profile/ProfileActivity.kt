964d0389fd3105aa1ab412c3c196aec839aacc44
Roubert Edgar <roubertedgar@gmail.com>
2019-08-02 23:09:04 -0300

    Creates Profile Activity

---
+++ app/src/main/java/com/downstairs/profile/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.downstairs.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
