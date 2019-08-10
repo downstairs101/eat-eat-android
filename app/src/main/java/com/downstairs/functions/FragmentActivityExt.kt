aba938eb7c16e8d8772ba6f599178a58fa89058b
Roubert Edgar <roubertedgar@gmail.com>
2019-08-10 18:42:28 -0300

    Removes unused files

--- app/src/main/java/com/downstairs/functions/FragmentActivityExt.kt
+++
@@ -1,7 +0,0 @@
-package com.downstairs.functions
-
-import androidx.fragment.app.FragmentActivity
-
-fun FragmentActivity.displayHeight() = DisplayTools
-        .getInstance(windowManager)
-        .displayHeightInPixel()
