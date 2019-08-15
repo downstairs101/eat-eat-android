f5d5c000afb4d9980f755c7b0aa800ee7d382cd1
Roubert Edgar <roubertedgar@gmail.com>
2019-08-15 20:00:19 -0300

    Injects place list view model into place list fragment

--- app/src/main/java/com/downstairs/place/list/PlaceListFragment.kt
+++ app/src/main/java/com/downstairs/place/list/PlaceListFragment.kt
@@ -6,18 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.downstairs.R
 import com.downstairs.place.details.PlaceDetailsActivity
+import com.downstairs.place.details.PlaceDetailsViewModel
 import kotlinx.android.synthetic.main.place_list_fragment.*
+import javax.inject.Inject
 
 class PlaceListFragment : Fragment() {
+    @Inject
+    lateinit var factory: ViewModelProvider.Factory
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, factory).get(PlaceListVIewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.place_list_fragment, container, false)
     }
 
