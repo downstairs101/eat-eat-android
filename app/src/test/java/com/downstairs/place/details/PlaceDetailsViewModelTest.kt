6d0a4102d3b6ef27515a00cfe0ade24eed3edc92
Roubert Edgar <roubertedgar@gmail.com>
2019-09-10 20:06:12 -0300

    Starts to test place details view model

---
+++ app/src/test/java/com/downstairs/place/details/PlaceDetailsViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.downstairs.place.details
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class PlaceDetailsViewModelTest {
+
+    private lateinit var viewModel: PlaceDetailsViewModel
+
+    @BeforeEach
+    internal fun setUp() {
+    }
+
+    @Test
+    internal fun `fetch place when place id is not null`() {
+        assertEquals("", "")
+    }
+}
