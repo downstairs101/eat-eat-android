b5d93a18d4bb19760c1fd529f441d13b4668035d
Roubert Edgar <roubertedgar@gmail.com>
2019-10-06 23:52:54 -0300

    Creates moshi with UUID adapter

--- app/src/main/java/com/downstairs/data/RetrofitClient.kt
+++ app/src/main/java/com/downstairs/data/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.downstairs.data
 
+import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -8,9 +9,11 @@ import javax.inject.Inject
 class RetrofitClient @Inject constructor(private val interceptor: HttpRequestInterceptor) {
 
     init {
+        val moshi = Moshi.Builder().add(UUIDAdapter()).build()
+
         Retrofit.Builder()
             .baseUrl("http://localhost.com")
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .client(buildClient())
     }
 
