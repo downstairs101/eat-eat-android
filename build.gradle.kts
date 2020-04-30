88df0e334a79071caab396d694817c642368e9d6
Roubert Edgar <roubertedgar@gmail.com>
2020-04-30 20:02:41 -0300

    Formats gradle from project level

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ buildscript {
     repositories {
         google()
         jcenter()
-
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
