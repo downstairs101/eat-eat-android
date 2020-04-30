7f56c0c24a2ff4cda3b76e68c29ebca6ab98159f
Roubert Edgar <roubertedgar@gmail.com>
2020-04-30 02:08:44 -0300

    Renames UserSplitsFragment to SplitsFragment

--- app/src/main/java/com/downstairs/UserSplitsFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.downstairs
-
-import androidx.fragment.app.Fragment
-
-class UserSplitsFragment: Fragment(R.layout.user_splits_fragment)
---
+++ app/src/main/java/com/downstairs/split/SplitsFragment.kt
@@ -0,0 +1,6 @@
+package com.downstairs.split
+
+import androidx.fragment.app.Fragment
+import com.downstairs.R
+
+class SplitsFragment: Fragment(R.layout.splits_fragment)
