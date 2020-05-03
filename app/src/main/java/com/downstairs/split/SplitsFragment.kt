15ffe053419d84b29018a14869d4f7c45337ee22
Roubert Edgar <roubertedgar@gmail.com>
2020-05-02 22:18:04 -0300

    Setup bottom sheet themes and behavior on base bottom sheet class

--- app/src/main/java/com/downstairs/split/SplitsFragment.kt
+++ app/src/main/java/com/downstairs/split/SplitsFragment.kt
@@ -1,13 +1,8 @@
 package com.downstairs.split
 
-import android.os.Bundle
-import android.view.View
 import com.downstairs.R
 import com.downstairs.eatat.core.components.BottomSheetFragment
 
 class SplitsFragment : BottomSheetFragment(R.layout.splits_fragment) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-    }
 }
--- core/src/main/java/com/downstairs/eatat/core/components/BottomSheetFragment.kt
+++ core/src/main/java/com/downstairs/eatat/core/components/BottomSheetFragment.kt
@@ -1,11 +1,14 @@
 package com.downstairs.eatat.core.components
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
-import com.downstairs.eatat.core.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 open class BottomSheetFragment(@LayoutRes private val layout: Int?) : BottomSheetDialogFragment() {
@@ -19,4 +22,14 @@ open class BottomSheetFragment(@LayoutRes private val layout: Int?) : BottomShee
             inflater.inflate(layout, container, false)
         }
     }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        onCreateBehavior(dialog.behavior)
+
+        return dialog
+    }
+
+    fun onCreateBehavior(behavior: BottomSheetBehavior<FrameLayout>) {}
+
 }
