735ec66e1b405232a9ccee572a606436472b41d3
Roubert Edgar <roubertedgar@gmail.com>
2020-05-03 03:09:36 -0300

    Changes split fragment, from bottom sheet to defaul fragment

--- app/src/main/java/com/downstairs/split/SplitsFragment.kt
+++ app/src/main/java/com/downstairs/split/SplitsFragment.kt
@@ -1,15 +1,13 @@
 package com.downstairs.split
 
-import android.widget.FrameLayout
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.downstairs.R
-import com.downstairs.eatat.core.components.BottomSheetFragment
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class SplitsFragment : BottomSheetFragment(R.layout.splits_fragment) {
+class SplitsFragment : Fragment(R.layout.splits_fragment) {
 
-    override fun onCreateBehavior(behavior: BottomSheetBehavior<FrameLayout>) {
-        behavior.isFitToContents = false
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
-
-    override fun getFragmentTag() = "SplitsFragment"
 }
