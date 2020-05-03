052f1a091928bf898bae79d78ac5e34d9caa57c0
Roubert Edgar <roubertedgar@gmail.com>
2020-05-02 23:19:43 -0300

    Themming navigation bar color for api 27plus

--- app/src/main/java/com/downstairs/split/SplitsFragment.kt
+++ app/src/main/java/com/downstairs/split/SplitsFragment.kt
@@ -1,8 +1,13 @@
 package com.downstairs.split
 
+import android.widget.FrameLayout
 import com.downstairs.R
 import com.downstairs.eatat.core.components.BottomSheetFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class SplitsFragment : BottomSheetFragment(R.layout.splits_fragment) {
 
+    override fun onCreateBehavior(behavior: BottomSheetBehavior<FrameLayout>) {
+        behavior.isFitToContents = false
+    }
 }
