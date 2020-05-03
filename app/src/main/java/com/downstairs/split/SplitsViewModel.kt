23a53b5a8e693f33202e47f9fd64eeedde141aee
Roubert Edgar <roubertedgar@gmail.com>
2020-05-03 18:48:31 -0300

    Posts mocket split list on init splits view model

--- app/src/main/java/com/downstairs/split/SplitsViewModel.kt
+++ app/src/main/java/com/downstairs/split/SplitsViewModel.kt
@@ -9,4 +9,19 @@ class SplitsViewModel @Inject constructor() : ViewModel() {
 
     private val mutableSplits = MutableLiveData<List<SplitUiModel>>()
     val splits: LiveData<List<SplitUiModel>> = mutableSplits
+
+    init {
+        loadSplits()
+    }
+
+    private fun loadSplits() {
+        val splits = listOf(
+            SplitUiModel("Edgar", "R$ 350,00"),
+            SplitUiModel("Allan Moreira", "R$ 150,00"),
+            SplitUiModel("Zezinho da esquina", "R$ 550,00"),
+            SplitUiModel("Seu João", "R$ 330,00")
+        )
+
+        mutableSplits.postValue(splits)
+    }
 }
