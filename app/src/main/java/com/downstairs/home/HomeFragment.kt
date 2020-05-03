f65b22edfc17311ef33064fb254c0803f0df9660
Roubert Edgar <roubertedgar@gmail.com>
2020-05-03 03:27:30 -0300

    Removes bottom sheet trash from home fragment

--- app/src/main/java/com/downstairs/home/HomeFragment.kt
+++ app/src/main/java/com/downstairs/home/HomeFragment.kt
@@ -1,15 +1,12 @@
 package com.downstairs.home
 
-import android.database.sqlite.SQLiteStatement
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.downstairs.R
-import com.downstairs.split.SplitsFragment
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        SplitsFragment().show(childFragmentManager)
     }
 }
