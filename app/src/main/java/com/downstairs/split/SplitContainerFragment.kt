4ca55d822a9fb3b181045f3aa930bc9cdd6d1958
Roubert Edgar <roubertedgar@gmail.com>
2020-05-09 23:33:06 -0300

    Set start app navigation as split nav graph

--- app/src/main/java/com/downstairs/split/SplitContainerFragment.kt
+++ app/src/main/java/com/downstairs/split/SplitContainerFragment.kt
@@ -3,4 +3,6 @@ package com.downstairs.split
 import androidx.fragment.app.Fragment
 import com.downstairs.R
 
-class SplitContainerFragment : Fragment(R.layout.split_container_fragment)
+class SplitContainerFragment : Fragment(R.layout.split_container_fragment) {
+
+}
--- app/src/main/java/com/downstairs/split/details/SplitDetailsFragment.kt
+++ app/src/main/java/com/downstairs/split/details/SplitDetailsFragment.kt
@@ -1,8 +1,15 @@
 package com.downstairs.split.details
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.downstairs.R
 
 class SplitDetailsFragment : Fragment(R.layout.split_details_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
 
 }
