51c45fb9c121c14aec78209d1d1e4c63aed08cdb
Roubert Edgar <roubertedgar@gmail.com>
2020-05-14 13:09:02 -0300

    pass bundled args to android navigation on SplitsFragment

--- app/src/main/java/com/downstairs/split/list/SplitsFragment.kt
+++ app/src/main/java/com/downstairs/split/list/SplitsFragment.kt
@@ -3,7 +3,6 @@ package com.downstairs.split.list
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -15,6 +14,7 @@ import com.downstairs.eatat.core.extensions.getCoreComponent
 import com.downstairs.eatat.core.tools.Instruction
 import com.downstairs.eatat.core.tools.Navigation
 import com.downstairs.eatat.core.tools.State
+import com.downstairs.eatat.core.tools.bundledArgs
 import com.downstairs.split.injection.DaggerSplitComponent
 import kotlinx.android.synthetic.main.splits_fragment.*
 import javax.inject.Inject
@@ -87,7 +87,7 @@ class SplitsFragment : Fragment(R.layout.splits_fragment) {
     }
 
     private fun navigate(navigation: Navigation) {
-        findNavController().navigate(navigation.destination, bundleOf(*navigation.arguments))
+        findNavController().navigate(navigation.destination, navigation.bundledArgs)
     }
 
     private fun freeViews() {
