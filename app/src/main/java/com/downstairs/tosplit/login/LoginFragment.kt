3c65c81ddb68eb66013ecb9936838cc21fd5c0cf
Roubert Edgar <roubertedgar@gmail.com>
2020-08-10 01:50:12 -0300

    Navigate to home on login

--- app/src/main/java/com/downstairs/tosplit/login/LoginFragment.kt
+++ app/src/main/java/com/downstairs/tosplit/login/LoginFragment.kt
@@ -5,9 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.downstairs.core.auth.AuthResultData
 import com.downstairs.core.auth.GoogleAuth
 import com.downstairs.core.extensions.getCoreComponent
+import com.downstairs.core.extensions.navigate
+import com.downstairs.core.tools.Instruction
+import com.downstairs.core.tools.Navigation
 import com.downstairs.tosplit.R
 import com.downstairs.tosplit.injection.DaggerAppComponent
 import kotlinx.android.synthetic.main.login_fragment.*
@@ -29,11 +34,22 @@ class LoginFragment : Fragment(R.layout.login_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupListeners()
+        setupObservers()
     }
 
     private fun setupListeners() {
         loginButton.setOnClickListener {
             viewModel.signIn(GoogleAuth(this))
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.viewInstruction.observe(viewLifecycleOwner, Observer { onInstructionChange(it) })
+    }
+
+    private fun onInstructionChange(instruction: Instruction) {
+        when (instruction) {
+            is Navigation -> findNavController().navigate(instruction)
         }
     }
 
--- app/src/main/java/com/downstairs/tosplit/login/LoginViewModel.kt
+++ app/src/main/java/com/downstairs/tosplit/login/LoginViewModel.kt
@@ -1,26 +1,54 @@
 package com.downstairs.tosplit.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.downstairs.core.auth.AuthInteractor
 import com.downstairs.core.auth.AuthMethod
+import com.downstairs.core.auth.AuthResult
 import com.downstairs.core.auth.AuthResultData
 import com.downstairs.core.extensions.launchIO
+import com.downstairs.core.tools.Instruction
+import com.downstairs.core.tools.Navigation
+import com.downstairs.core.tools.ViewInstruction
+import com.downstairs.tosplit.R
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor(private val authInteractor: AuthInteractor) : ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val instruction: LoginViewInstruction,
+    private val authInteractor: AuthInteractor
+) : ViewModel() {
 
-
+    private val _viewInstruction = MutableLiveData<Instruction>()
+    val viewInstruction: LiveData<Instruction> = _viewInstruction
 
     fun signIn(authMethod: AuthMethod) {
         viewModelScope.launchIO {
             val result = authInteractor.authorize(authMethod)
+
+            if (result is AuthResult.Authorized) {
+                authorizedUser()
+            } else {
+                unauthorizedUser()
+            }
         }
+    }
+
+    private fun authorizedUser() {
+        _viewInstruction.postValue(instruction.navigateToHome())
+    }
+
+    private fun unauthorizedUser() {
+
     }
 
     fun processSignInResult(authResultData: AuthResultData) {
         authInteractor.processAuthResult(authResultData)
     }
+}
 
+class LoginViewInstruction @Inject constructor() : ViewInstruction() {
 
+    fun navigateToHome() = Navigation(R.id.fromLoginToHome)
 }
