a74ed4cdd9c190b1c7e3cbfb4591f5fd4d1d9720
Roubert Edgar <roubertedgar@gmail.com>
2020-08-29 00:03:05 -0300

    Uses debug keystore as default for artifact signin

--- buildSrc/src/main/kotlin/Signed.kt
+++ buildSrc/src/main/kotlin/Signed.kt
@@ -1,16 +1,43 @@
 import java.io.File
 
 object Release {
-    val keystoreFile = File(System.getenv("RELEASE_KEYSTORE_FILE_PATH") ?: "")
-    val keystorePass: String = System.getenv("RELEASE_KEYSTORE_PASS")
-    val keyAlias: String = System.getenv("RELEASE_KEY_ALIAS")
-    val keyPass: String = System.getenv("RELEASE_KEY_PASS")
-
+    val keystoreFile = getKeystoreFile("RELEASE_KEYSTORE_FILE_PATH")
+    val keystorePass = getKeystorePass("RELEASE_KEYSTORE_PASS")
+    val keyAlias = getKeystoreAlias("RELEASE_KEY_ALIAS")
+    val keyPass = getKeyPass("RELEASE_KEY_PASS")
 }
 
 object Development {
-    val keystoreFile = File(System.getenv("DEV_KEYSTORE_FILE_PATH") ?: "")
-    val keystorePass: String = System.getenv("DEV_KEYSTORE_PASS")
-    val keyAlias: String = System.getenv("DEV_KEY_ALIAS")
-    val keyPass: String = System.getenv("DEV_KEY_PASS")
+    val keystoreFile = getKeystoreFile("DEV_KEYSTORE_FILE_PATH")
+    val keystorePass = getKeystorePass("DEV_KEYSTORE_PASS")
+    val keyAlias = getKeystoreAlias("DEV_KEY_ALIAS")
+    val keyPass = getKeyPass("DEV_KEY_PASS")
 }
+
+private fun getKeystoreFile(variable: String): File {
+
+    val path = System.getProperty(variable) ?: "${System.getProperty("HOME")}/.android/debug.keystore"
+
+    if (path.isNullOrBlank()) {
+        throw KeystoreNotFound(path)
+    }
+
+    return File(path)
+}
+
+private fun getKeystorePass(variable: String): String {
+    return System.getenv(variable) ?: "android"
+}
+
+private fun getKeystoreAlias(variable: String): String {
+    return System.getenv(variable) ?: "androiddebugkey"
+
+}
+
+private fun getKeyPass(variable: String): String {
+    return System.getenv(variable) ?: "android"
+}
+
+
+class KeystoreNotFound(filePath: String?) :
+    RuntimeException("Keystore file not found for path $filePath")
