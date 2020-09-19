c2df3c17ee98eadb9741cd58db38d333957fbcaa
Roubert Edgar <roubertedgar@gmail.com>
2020-09-19 18:45:43 -0300

    Removes unused http manager injection on main activity

--- app/src/main/java/com/downstairs/tosplit/MainActivity.kt
+++ app/src/main/java/com/downstairs/tosplit/MainActivity.kt
@@ -5,14 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.downstairs.core.extensions.getCoreComponent
 import com.downstairs.core.extensions.navigateUp
-import com.downstairs.core.http.HttpManager
 import com.downstairs.tosplit.injection.DaggerAppComponent
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
-
-    @Inject
-    lateinit var httpManager: HttpManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         DaggerAppComponent.factory()
@@ -20,8 +15,6 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
             .inject(this)
 
         super.onCreate(savedInstanceState)
-
-        print(httpManager)
     }
 
     override fun onBackPressed() {
