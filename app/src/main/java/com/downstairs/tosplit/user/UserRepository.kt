4c096b1db0e87ef570cfdc3eae46efac691b4391
Roubert Edgar <roubertedgar@gmail.com>
2020-09-22 23:10:55 -0300

    Formats UserRepository

--- app/src/main/java/com/downstairs/tosplit/user/UserRepository.kt
+++ app/src/main/java/com/downstairs/tosplit/user/UserRepository.kt
@@ -3,6 +3,7 @@ package com.downstairs.tosplit.user
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(private val userService: UserService) {
+
     suspend fun getUser(): User {
         return userService.getUser().toDomain()
     }
