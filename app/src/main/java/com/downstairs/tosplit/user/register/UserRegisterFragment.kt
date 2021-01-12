e30d6e9ec21f36518eb92357b8dbc8b517736fe8
Roubert Edgar <roubertedgar@gmail.com>
2021-01-11 23:27:51 -0300

    Creates user register fragment

---
+++ app/src/main/java/com/downstairs/tosplit/user/register/UserRegisterFragment.kt
@@ -0,0 +1,8 @@
+package com.downstairs.tosplit.user.register
+
+import androidx.fragment.app.Fragment
+import com.downstairs.tosplit.R
+
+class UserRegisterFragment : Fragment(R.layout.user_register_fragment) {
+
+}
